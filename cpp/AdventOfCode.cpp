fdbfdc16724d4340140b7d4dd34023f4ea387e34
Lukas R <lukas.rustemeyer@fluppisoft.com>
2025-12-07 22:49:17 +0100

    - Solve day 7 part 2 in a way that actually terminates in finite time

--- cpp/AdventOfCode.cpp
+++ cpp/AdventOfCode.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[])
 {
     aoc::assert_result(std::make_tuple(21ull, 40ull), aoc::day7::solve(true));
-    aoc::assert_result(std::make_tuple(1660ull, 0ull), aoc::day7::solve(false));
+    aoc::assert_result(std::make_tuple(1660ull, 305999729392659ull), aoc::day7::solve(false));
     return 0;
 }
--- cpp/y2025/day7.h
+++ cpp/y2025/day7.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <numeric>
+
 #include "../parser.h"
 #include "../views.h"
 
@@ -7,84 +9,64 @@
 
 namespace aoc::day7
 {
-    struct Params
-    {
-        std::string diagram;
-        unsigned long long num_lines = 0ull;
-        unsigned long long line_width = 0ull;
-    };
-
-    inline unsigned long long simulate_beams(const Params& params, std::string prev_line, const unsigned long long start_row, const bool p2)
-    {
-        auto result = 0ull;
-        for (auto row_idx = start_row; row_idx < params.num_lines; ++row_idx)
-        {
-            auto new_line = params.diagram.substr(row_idx * params.line_width, params.line_width);
-            for (auto column_idx = 0ull; column_idx < params.line_width; ++column_idx)
-            {
-                if (prev_line[column_idx] != 'S')
-                {
-                    continue;
-                }
-
-                if (new_line[column_idx] == '^')
-                {
-                    result += 1;
-
-                    if (p2)
-                    {
-                        new_line[column_idx - 1] = 'S';
-                        result += simulate_beams(params, new_line, row_idx + 1, p2);
-                        new_line[column_idx - 1] = '.';
-
-                        new_line[column_idx + 1] = 'S';
-                        result += simulate_beams(params, new_line, row_idx + 1, p2);
-
-                        return result;
-                    }
-
-                    new_line[column_idx - 1] = 'S';
-                    new_line[column_idx + 1] = 'S';
-                }
-                else
-                {
-                    new_line[column_idx] = 'S';
-                }
-            }
-
-            prev_line = std::move(new_line);
-        }
-
-        return result;
-    };
-
     inline auto solve(const bool is_test)
     {
         auto file = load_input_file(2025, 7, is_test);
 
         // Build the diagram
-        auto params = Params();
-        auto first_line = std::string();
+        auto diagram = std::vector<long long>();
+        auto num_rows = 0ull;
+        auto num_columns = 0ull;
+
         auto line = std::string();
         while (std::getline(file, line))
         {
-            if (first_line.empty())
+            diagram.append_range(line | std::views::transform([](const char c)
             {
-                first_line = line;
-            }
-            else
-            {
-                params.diagram.append_range(line);
-                params.line_width = line.size();
-                ++params.num_lines;
-            }
+                return c == 'S' ? 1 : c == '^' ? -1 : 0;
+            }));
+            ++num_rows;
+            num_columns = line.size();
         }
 
-        // Calculate part 1
-        auto result_p1 = simulate_beams(params, first_line, 0, false);
+        // Calculate part 1 (and do the groundwork for part 2)
+        auto result_p1 = 0ull;
+        for (auto row_idx = 0ull; row_idx < num_rows - 1; ++row_idx)
+        {
+            const auto row = std::span(diagram.begin() + row_idx * num_columns, diagram.begin() + (row_idx + 1) * num_columns);
+            auto next_row = std::span(diagram.begin() + (row_idx + 1) * num_columns, diagram.begin() + (row_idx + 2) * num_columns);
+            for (auto column_idx = 0ull; column_idx < num_columns; ++column_idx)
+            {
+                const auto count = row[column_idx];
+                if (count < 1)
+                {
+                    continue;
+                }
+
+                if (next_row[column_idx] == -1)
+                {
+                    // The beam splits, add to the number of paths that fields below can be reached from
+                    next_row[column_idx - 1] += count;
+                    next_row[column_idx + 1] += count;
+                    result_p1 += 1;
+                }
+                else
+                {
+                    // Beam isn't split, keep the number of timelines
+                    next_row[column_idx] += count;
+                }
+            }
+
+            const auto row_range = next_row | std::views::transform([](const auto i)
+            {
+                return i < 0ll ? '^' : i > 0ll ? '0' + static_cast<char>(std::min(i, 9ll)) : '.';
+            });
+            std::cout << std::string(row_range.begin(), row_range.end()) << "\n";
+        }
 
         // Calculate part 2
-        auto result_p2 = simulate_beams(params, first_line, 0, true) + 1;
+        const auto last_row = std::span(diagram.end() - num_columns, diagram.end());
+        const auto result_p2 = std::accumulate(last_row.begin(), last_row.end(), 0ull);
 
         return std::make_tuple(result_p1, result_p2);
     }
